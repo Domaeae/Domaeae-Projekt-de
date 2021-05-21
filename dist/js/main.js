75fa0b7091cad40d8926c800fee0a8bbacf58248
Domaeae <DominikGirrbach@live.de>
2021-05-21 22:09:39 +0200

    +1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,21 @@
 $(function () {
   const btn = $(".form-button");
   const btn_submit = $('.form-submit');
+  const btn_md = $('.btn-md');
+  const modal = $('.modal-section');
+
+
+  /* <== EVENT| Button click
+    | Waiting 0,5s for Ripple ==>*/
+  // TODO: ...
+  let vheight = $(window).height();
+  let vwidth = $(window).width();
+  console.log(vheight);
+  console.log(vwidth);
+  modal.css({
+    "display": "block"
+  });
+
 
   function toggleClass() {
     this.classList.toggle("active");
@@ -8,7 +23,7 @@ $(function () {
 
   function addClass() {
     this.classList.add("finished");
-    btn_submit.text("");
+    btn_md.text("");
     console.log("Message send!");
   }
 
