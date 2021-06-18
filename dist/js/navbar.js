064347b87457578971f5fe6c12e2dd20a6d730a7
Domaeae <DominikGirrbach@live.de>
2021-06-18 23:41:07 +0200

    Update JS + Some new UI-Features
    + Portfolio
    +Manifest
    + JS-> Navbar Fixed
    + Some other Stuff

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -1,24 +1,39 @@
 $(document).ready(function () {
+  // 📈 | Constante - Variablen
+  const $nav_list = $('.nav-list');
 
-  $(function () {
-    $("nav ul li a:not(:only-child)").click(function (e) {
-
-      $(this).siblings(".nav-dropdown").toggle();
-      $(".nav-dropdown").not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-
-    $("html").click(function () {
-      $(".nav-dropdown").hide();
-    });
-    $("#nav-toggle").click(function () {
-      $("nav ul").slideToggle();
-    });
-    $("#nav-toggle").on("click", function () {
-      this.classList.toggle("active");
-    });
+  // 📎 | Variablen
+  var $nav_contact = $('#contact');
+  var $nav_portfolio = $('#portfolio');
+  var $nav_member = $('#member');
 
 
+
+  $nav_contact.click(function (e) {
+    e.preventDefault();
+    window.open('../../sites/kontakt.html');
   });
+
+
+
+
+  // $("nav ul li a:not(:only-child)").click(function (e) {
+
+  //   $(this).siblings(".nav-dropdown").toggle();
+  //   $(".nav-dropdown").not($(this).siblings()).hide();
+  //   e.stopPropagation();
+  // });
+
+  // $("html").click(function () {
+  //   $(".nav-dropdown").hide();
+  // });
+
+  // $("#nav-toggle").click(function () {
+  //   $("nav ul").slideToggle();
+  // });
+
+  // $("#nav-toggle").on("click", function () {
+  //   this.classList.toggle("active");
+  // });
 
 });
